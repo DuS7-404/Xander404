aa3f17f45260a6ed04040465a745fe4a7cc66697
Hobo Noir <hobonetics@gmail.com>
2016-12-06 23:40:18 -0500

    fixed buttns

--- app/src/main/java/com/example/lulzerver/betterxander/MainActivity.java
+++ app/src/main/java/com/example/lulzerver/betterxander/MainActivity.java
@@ -6,48 +6,81 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
     TextView txt;
     Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    txt=(TextView) findViewById(R.id.textView2);
-    btn=(Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener(){
-         @Override
-            public void onClick(View v){
-             txt.setText("Now my superiors are attempting to get me to say Hello World, but I will tell you one thing, click button 2 to see the secrets.");
-         }
+        txt = (TextView) findViewById(R.id.textView2);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setText("Now my superiors are attempting to get me to say Hello World, but I will tell you one thing, click button 2 to see the secrets.");
+            }
 
         });
 
 
     }
 
-
-public void trig2(){
-    txt=(TextView)findViewById(R.id.textView2);
-    btn=(Button)findViewById(R.id.button2);
-        btn.setOnClickListener(new View.OnClickListener(){
-@Override
-                public void onClick(View v){
-                txt.setText("Now are you really ready for the secret ? If so press the 3rd and final button.");
-            }
-        });
-
-
-
-}
-    public void onCreate(){
-        txt=(TextView)findViewById(R.id.textView2);
-        btn=(Button)findViewById(R.id.button3);
-        btn.setOnClickListener(new View.OnClickListener(){
-@Override
-            public void onClick(View v){
-                txt.setText("No Mans Sky was a dissappointment soley due to the fact that it was not multiplayer.");
-            }
-        });
+    /**
+     * Turn a light off
+     */
+    public void turnOff(View view) {
+        sendRequest("off");
     }
+
+    /**
+     * Turn a light on
+     */
+    public void turnOn(View view) {
+        sendRequest("on");
+    }
+
+    public void sendRequest(String state)  {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://requestb.in/1malqqd1";
+
+        //define json object
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("state", state);
+        } catch (JSONException e) {
+            System.out.println("YOU HAVE INCORRECT JSON");
+        }
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.POST, url, jsonBody, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+
+                    }
+                });
+// Add the request to the RequestQueue.
+        queue.add(jsObjRequest);
+    }
+
+
 }
